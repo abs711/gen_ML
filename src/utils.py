77b1bf3b1463817592705f0022a1d5dde363d6c1
Abhishek Sharma <abhisheksharma0711@gmail.com>
2023-07-29 19:32:30 -0700

    trainer implemented

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,28 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train,y_train,X_test,y_test,models,score_fn):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_pred_tr = model.predict(X_train)
+
+            y_pred_ts = model.predict(X_test)
+
+            train_score = score_fn(y_train, y_pred_tr)
+            test_score = score_fn(y_test, y_pred_ts)
+
+            report[list(models.keys())[i]] = test_score
+            
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
